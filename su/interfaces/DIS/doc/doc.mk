72040068c6fd4411e68db0c6ef5562c4531dee99
collartm <matt_1979@hotmail.fr>
2016-10-05 16:40:23 +0200

    ULOGNA_N2_AS
    AS

---
+++ su/interfaces/DIS/doc/doc.mk
@@ -0,0 +1,7 @@
+#!/usr/bin/make -f
+#  @(#)  Automatically produced by $Id: genmk.pl 798 2007-12-27 12:40:06Z bdav $ from $Id: templatedoc.mk 503 2007-01-23 10:10:10Z bdav $
+#
+# ORACLE     Copyright SYDEL  LORIENT-FRANCE
+#
+
+include $(BUILD_TOOLS)/doc/doc.mk
